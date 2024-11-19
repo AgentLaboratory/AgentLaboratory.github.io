597a645fa2b0f0ca34f5070e4464484dce2c540a
samuel schmidgall <samuelschmidgall@macbook-pro.mynetworksettings.com>
2024-11-18 23:27:40 -0500

    updates

--- static/js/index.js
+++ static/js/index.js
@@ -37,6 +37,7 @@ $(document).ready(function() {
     });
     // Get the YouTube iframe
     var youtubeFrame = $('#youtube-video-frame');
+    
 
     // Function to calculate the center and bounds of the iframe
     function isMouseNearCenter(event) {
